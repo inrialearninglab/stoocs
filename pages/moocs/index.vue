f9bcdca8e2acd35fdbc4940d5b4efa0abc876384
VIAUD Nathan <nathan.viaud@inria.fr>
2024-06-14 11:52:08 +0200

    feat: moocs filter

--- pages/moocs/index.vue
+++ pages/moocs/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { useMoocs } from '~/stores/moocs.store';
+import { Search } from 'lucide-vue-next';
 
 const moocsStore = useMoocs()
 
@@ -9,7 +10,14 @@ onMounted(() => {
 </script>
 
 <template>
-    <div>
-        <MoocList :moocs="moocsStore.moocs" />
+    <div class="flex flex-col gap-5">
+        <div class="relative w-full max-w-sm items-center">
+            <Input id="search" v-model="moocsStore.search" placeholder="Chercher un mooc..." class="pl-8"/>
+            <span class="absolute start-0 inset-y-0 flex items-center justify-center px-2">
+                <Search class="size-5 text-muted-foreground" />
+            </span>
+        </div>
+
+        <MoocList :moocs="moocsStore.filteredMoocs" />
     </div>
 </template>
--- stores/moocs.store.ts
+++ stores/moocs.store.ts
@@ -3,12 +3,20 @@ import { fetchMoocs } from '~/services/moocs.service';
 
 interface MoocsState {
     moocs: Mooc[];
+    search: string;
 }
 
 export const useMoocs = defineStore('moocs', {
     state: (): MoocsState => ({
-        moocs: []
+        moocs: [],
+        search: ''
     }),
+    
+    getters: {
+        filteredMoocs(): Mooc[] {
+            return this.moocs.filter(mooc => mooc.title.toLowerCase().includes(this.search.toLowerCase()));
+        }
+    },
     
     actions: {
         async getMoocs() {
