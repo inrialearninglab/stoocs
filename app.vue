082f2e5db0acdd9934c9695d0a8478256b2c87ac
VIAUD Nathan <nathan.viaud@inria.fr>
2024-06-27 11:52:14 +0200

    style: hero page

--- app.vue
+++ app.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import Toaster from '~/components/ui/toast/Toaster.vue'
 
+const route = useRoute()
+
 </script>
 
 <template>
     <div class="flex flex-col">
-        <Header />
-        <div class="p-8">
+        <Header v-if="route.path !== '/'" />
+        <div :class="{ 'p-8': route.path !== '/' }">
             <NuxtPage />
             <Toaster />
         </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,39 @@
 <template>
-    <div class="flex flex-col gap-12">
-        <h1>Welcome to 丂ｲののᄃ</h1>
+    <div class="relative overflow-hidden py-24 lg:py-32">
+        <div
+            aria-hidden="true"
+            class="flex absolute -top-96 start-1/2 transform -translate-x-1/2"
+        >
+            <div class="bg-gradient-to-r from-background/50 to-background blur-3xl w-[25rem] h-[44rem] rotate-[-60deg] transform -translate-x-[10rem]" />
+            <div class="bg-gradient-to-tl blur-3xl w-[90rem] h-[50rem] rounded-full origin-top-left -rotate-12 -translate-x-[15rem] from-primary-foreground via-primary-foreground to-background" />
+        </div>
+        <div class="relative z-10">
+            <div class="container py-10 lg:py-16">
+                <div class="max-w-2xl text-center mx-auto">
+                    <p class="">By Nathan</p>
+                    <div class="mt-5 max-w-2xl">
+                        <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+                            丂ｲののᄃ
+                        </h1>
+                    </div>
+                    <div class="mt-5 max-w-3xl">
+                        <p class="text-xl text-muted-foreground">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                            do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                    </div>
+                    <div class="mt-8 gap-3 flex justify-center">
+                        <Button size="lg" as-child>
+                            <NuxtLink to="/moocs">
+                                Commencer
+                            </NuxtLink>
+                        </Button>
+                        <Button size="lg" variant="outline">
+                            Documentation
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
