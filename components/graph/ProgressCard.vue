351c0220ae070f0d8bb4325ffd3d2db93a57dca4
VIAUD Nathan <nathan.viaud@inria.fr>
2024-07-11 11:27:17 +0200

    styles: change progressCard style

--- components/graph/ProgressCard.vue
+++ components/graph/ProgressCard.vue
@@ -14,19 +14,22 @@ defineProps<{
 </script>
 
 <template>
-    <Card class="max-w-64">
+    <Card class="max-w-64 flex flex-col">
         <CardHeader class="relative">
             <div class="flex gap-5 justify-between">
-                <CardTitle>{{ title }} ({{ dividend }})</CardTitle>
+                <CardTitle>{{ title }}</CardTitle>
                 <Component :is="icon" class="size-4 text-muted-foreground" />
             </div>
             <CardDescription>{{ description }}</CardDescription>
             <GraphReportChip report="grade" />
         </CardHeader>
-        <CardContent class="justify-center flex">
+        <CardContent class="justify-center flex flex-1 items-end">
             <Skeleton v-if="loading" class="size-32 rounded-full" />
-            <GraphRadialProgress v-else-if="dividend && divisor" :dividend="dividend" :divisor="divisor" class="w-32"/>
+            <GraphRadialProgress v-else-if="dividend && divisor" :dividend="dividend" :divisor="divisor" class="size-32"/>
             <div v-else class="text-[8rem]">🥸</div>
         </CardContent>
+        <CardFooter>
+            <p class="text-xl text-center w-full">{{ dividend?.toLocaleString('fr-FR') }} Utilisateurs</p>
+        </CardFooter>
     </Card>
 </template>
