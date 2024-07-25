9f9f4dab4b3b04c670ac95230c5989c41460c671
VIAUD Nathan <nathan.viaud@inria.fr>
2024-07-25 17:48:02 +0200

    fix: auth guards

--- middleware/02.guard.global.ts
+++ middleware/02.guard.global.ts
@@ -1,11 +1,19 @@
 export default defineNuxtRouteMiddleware(async (to) => {
     const guestRoutes = [
         '/auth/login',
-        '/'
+        '/auth/register/*',
     ];
     
     const user = useUser();
-    if (!user.value && !guestRoutes.includes(to.path)) {
+    
+    const matchesGuestRoute = (route: string) => {
+        return guestRoutes.some(guestRoute => {
+            const regex = new RegExp(`^${guestRoute.replace(/\*/g, '.*')}$`);
+            return regex.test(route);
+        });
+    };
+    
+    if (!user.value && !matchesGuestRoute(to.path)) {
         return navigateTo('/auth/login');
     }
 });
--- server/middleware/02.guard.ts
+++ server/middleware/02.guard.ts
@@ -1,7 +1,9 @@
 export default defineEventHandler(async (event) => {
     const guestRoutes = [
         { url: '/api/auth/login', method: 'POST' },
-        { url: '/api/auth/user', method: 'GET' }
+        { url: '/api/auth/user', method: 'GET' },
+        { url: '/api/auth/register', method: 'POST' },
+        { url: '/api/auth/invitations/email', method: 'POST' },
     ];
    
     if (event.path.startsWith('/api')) {
