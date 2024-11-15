ef4beee7a88f8b221b7e4ff91c61e14eeac93130
VIAUD Nathan <nathan.viaud@inria.fr>
2024-11-15 14:03:19 +0100

    chores: update packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ import animate from 'tailwindcss-animate'
 import { setupInspiraUI } from '@inspira-ui/plugins'
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   darkMode: ["class"],
   safelist: [
       "dark",
