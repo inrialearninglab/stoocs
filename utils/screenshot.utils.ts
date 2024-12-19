f9049ef793e751b4a3e1f1590d468f36f73a4cdf
VIAUD Nathan <nathan.viaud@inria.fr>
2024-12-19 15:36:13 +0100

    improvement: add legend to screenshot

--- utils/screenshot.utils.ts
+++ utils/screenshot.utils.ts
@@ -1,24 +1,42 @@
 import { toast } from 'vue-sonner';
-import html2canvas from 'html2canvas';
+import { toPng } from 'html-to-image';
 
 export function saveChartAsPNG(id: string) {
-    const graph = document.getElementById(id) as HTMLElement;
-    if (!graph) {
+    const chart = document.getElementById(id) as HTMLElement;
+    const legend = document.getElementById(`${id}-legend`);
+    if (!chart) {
         toast.error("Erreur durant la capture d'écran");
 
         return;
     }
 
-    html2canvas(graph).then((canvas) => {
-        const link = document.createElement('a');
-        link.href = canvas.toDataURL('image/png');
-        link.download = `${id}.png`;
+    const graph = document.createElement('div');
+    if (legend) graph.appendChild(legend.cloneNode(true));
+    graph.appendChild(chart.cloneNode(true));
 
-        document.body.appendChild(link);
-        link.click();
+    graph.style.position = 'absolute';
+    graph.style.top = '0';
+    graph.style.left = '0';
+    graph.style.zIndex = '-9999';
 
-        document.body.removeChild(link);
-    });
+    document.body.appendChild(graph);
 
-    toast.success("Capture d'écran réalisé avec succès");
+    toPng(graph)
+        .then((dataUrl) => {
+            const image = new Image();
+            image.src = dataUrl;
+            console.log(image);
+            const link = document.createElement('a');
+            link.href = dataUrl;
+            link.download = `${id}.png`;
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+
+            document.body.removeChild(graph);
+        })
+        .catch((error) => {
+            console.error('error during screenshot');
+            toast.error("Erreur durant la capture d'écran");
+        });
 }
