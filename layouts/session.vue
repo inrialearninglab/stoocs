ae65ef6a53948e075564f369913504abd5d19d99
VIAUD Nathan <nathan.viaud@inria.fr>
2024-12-19 10:34:02 +0100

    style: fix style

--- layouts/session.vue
+++ layouts/session.vue
@@ -1,7 +1,7 @@
 <template>
     <LayoutHeader />
     <SessionLayoutHeader />
-    <div class="p-8 flex-1 flex-col">
+    <div class="p-8 flex-1 flex flex-col">
         <slot />
     </div>
 </template>
