2e088757732ee9b8d0c2820fcef453b5e2d8e99a
VIAUD Nathan <nathan.viaud@inria.fr>
2025-01-15 13:34:29 +0100

    fix: app initialisation

--- server/plugins/app-setup.ts
+++ server/plugins/app-setup.ts
@@ -3,7 +3,19 @@ import { prisma } from '~/prisma/db';
 export default defineNitroPlugin(async (nitroApp) => {
     let app = await prisma.app.findFirst();
     if (!app) {
-        app = await prisma.app.create();
+        app = await prisma.app.create({});
+
+        await prisma.role.create({
+            data: {
+                name: 'ILL',
+            },
+        });
+
+        await prisma.role.create({
+            data: {
+                name: 'Guest',
+            },
+        });
     }
 
     // @ts-ignore
