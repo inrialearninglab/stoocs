b376f9367e9482a33020161ff2757089862a502f
VIAUD Nathan <nathan.viaud@inria.fr>
2025-02-24 17:46:22 +0100

    chores: forgot to remove

--- components/metrics/charts/Enrollments.vue
+++ components/metrics/charts/Enrollments.vue
@@ -97,7 +97,7 @@ const totalChartId = 'enrollment-total-chart';
 
 const sessionStore = useSession();
 
-const title = `Inscriptions (${sessionStore.totalEnrollments?.toLocaleString('fr-FR')})`;
+const title = 'Inscriptions';
 const description = computed(
     () => `
         ${sessionStore.totalEnrollments?.toLocaleString('fr-FR')} inscriptions au total.
