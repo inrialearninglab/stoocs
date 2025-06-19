8fb085443e71ba8cc2a1e256c7be2958694707d9
VIAUD Nathan <nathan.viaud@inria.fr>
2025-06-19 14:24:40 +0200

    fix: forgot table footer

--- components/mooc/list/Index.vue
+++ components/mooc/list/Index.vue
@@ -9,5 +9,27 @@ const moocsStore = useMoocs();
         <template #filters="{ table }">
             <MoocFilter :table="table" />
         </template>
+
+        <template #footer>
+            <TableFooter>
+                <TableRow>
+                    <TableCell>Total</TableCell>
+                    <TableCell>{{ moocsStore.filteredMoocsCount }} MOOC</TableCell>
+                    <TableCell>{{ moocsStore.filteredSessions.length }} Sessions</TableCell>
+                    <TableCell>
+                        {{
+                            moocsStore.filteredSessions
+                                .reduce((acc, session) => acc + (session?.totalEnrollments ?? 0), 0)
+                                .toLocaleString('fr-FR')
+                        }}
+                    </TableCell>
+                    <TableCell></TableCell>
+                    <TableCell></TableCell>
+                    <TableCell></TableCell>
+                    <TableCell></TableCell>
+                    <TableCell></TableCell>
+                </TableRow>
+            </TableFooter>
+        </template>
     </DataTable>
 </template>
